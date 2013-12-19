3a97690894e5d5c866bf8d1fde99309bfc1b1ec5
Doddy-Sudarjad <nivvnalck@gmail.com>
2013-12-20 00:43:24 +0700

    Update tambah_wisata.php

--- tambah_wisata.php
+++ tambah_wisata.php
@@ -12,42 +12,67 @@ include 'header.php';
 			</div>
 
 			<div class="span9">
-				<div class="hero-unit">
+				<div class="tambahbrt">
+                	</div>
+                	
+                	<hr />
+          		<div class="hero-unit">
                 	<!-- start slide -->
-                    <button type="submit" class="btn btn-success">Tambah</button>
-                    <table border="1" width="100%">
+                    <form action="edit_Proses_Wisata.php" method="post" enctype="multipart/form-data">
+		    <table border="1" width="100%">
+		    
+		    <?php
+						$sql=mysql_query("select * from berita where user_id='$username' and id_berita='$id' ");
+						while ($h=mysql_fetch_array($sql))
+						{
+						
+					?>
+					
+					<tr>
+					<td><input name="id_berita" type="hidden" value="<?php echo $id;?>"  /></td>
+					</tr>
                     <tr>
-                    	<th>No</th>
-                        <th>Nama Wisata</th>
-                        <th>Deskripsi</th>
-                        <th>Foto</th>
+                    	<th>Judul</th>
+		    
+		    <td><input name="judul" type="text" id="judul" value="<?php echo $h['judul'];?>" required="required"  /></td>
                     </tr>
                     <tr>
-                   		<td>1</td>
-                    	<td>Danau Toba</td>
-                        <td>Deskripsi mengenai danau toba</td>
-                        <td align="center"><img src="img/foto1.jpg" class="img-wisata" /></td>
+                    	<th>Kategori</th>
+                        	<td>
+    	                        <SELECT NAME="kategori" WIDTH=250 > 
+	                                        <?php
+											$sql1=mysql_query("select * from kategori where id_kat='$h[id_kat]'");
+											while($ket=mysql_fetch_array($sql1)) 
+											{
+        	                                echo "<option value=0 selected>$ket[nama_kat]</option>";
+											}
+             	                            $perintah=("SELECT * FROM kategori where id_kat!='$h[id_kat]' order by nama_kat");
+                	                        $hasil=mysql_query($perintah);
+                    	                    while($r=mysql_fetch_array($hasil))
+                        	                {
+                            	            echo("<OPTION VALUE=$r[id_kat]>$r[nama_kat]</option>");
+                                	        }
+                                    	    echo "</option>";
+                                        	?>
+                             </SELECT>
+                        </td>
                     </tr>
+                    <tr>
+                    	<th>Gambar</th>
+                        <td><input name="fupload" type="file" size="50" /></td>
+                    </tr>
+                    <tr>
+                        	<td colspan="2"><input type="submit" value="Edit" class="btn btn-success"> <input class="btn btn-success" type="button" onClick="self.history.back()" value="Batal"></td>
+                          
+                        </tr>
+						<?php } ?>
                     </table>
+                    </form>
 				</div>
                
-               <!-- Pagination-->
-               <div class="pagination">
-					<ul>
-						<li class"disabled"><span>Prev</span></li>
-						<li class"disabled"><span>1</span></li>
-						<li><a href="#">2</a></li>
-						<li><a href="#">3</a></li>
-						<li><a href="#">4</a></li>
-						<li><a href="#">5</a></li>
-						<li><a href="#">Next</a></li>
-					</ul>
-				</div>
-				
 
 			</div>
 		</div>
-        <!-- akhir isi -->
 	</div>
 	
 	<hr />
