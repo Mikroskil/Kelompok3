0cfa5e86754aabe849df3a431025acd922fa33f2
DyanAgustin <dyanshb42@gmail.com>
2013-12-19 18:57:53 +0700

    update otentikasi baru

--- otentikasi.php
+++ otentikasi.php
@@ -9,22 +9,22 @@ function anti_injection($data){
 
 
 //tangkap data dari form login
-$email = $_POST['username'];
+$username = $_POST['username'];
 $password = $_POST['password'];
 $pass1 = md5($password);
-$pass=md5(md5($pass));
+$pass=md5(md5($pass1));
 
 //untuk mencegah sql injection
 //kita gunakan mysql_real_escape_string
-$email = anti_injection($email);
+$username = anti_injection($username);
 $pass = anti_injection($pass);
 
 //cek data yang dikirim, apakah kosong atau tidak
-if (empty($email) && empty($pass)) {
+if (empty($username) && empty($pass)) {
 	//kalau username dan password kosong
 	header('location:index.php?error=1');
 	break;
-} else if (empty($email)) {
+} else if (empty($username)) {
 	//kalau username saja yang kosong
 	header('location:index.php?error=2');
 	break;
@@ -42,11 +42,11 @@ if (!ctype_alnum($pass)){
  echo '<div class="errormsgbox" style="color: #D8000C;background-color: #FFBABA;background-image: url(img/error.png);background-repeat: no-repeat;background-position: 10px center;font-weight:bold;width:450px;border: 1px solid;margin: 0 auto;padding:10px 5px 10px 50px; ">Sekarang tidak bisa di injeksi halaman login nya bro <img src="img/senyum.gif"> </div>';
 }
 else {
-$q = mysql_query("select * from users where (email='$email' or username='$email') and password='$pass'");
+$q = mysql_query("select * from users where username='$username' and password='$pass'");
 $ketemu = mysql_num_rows($q);
 $r = mysql_fetch_array($q);
 $id_user=$r['id_user'];
-$email=$r['email'];
+$username=$r['username'];
 
 if ($ketemu == 1) {
 	//kalau username dan password sudah terdaftar di database
@@ -54,7 +54,7 @@ if ($ketemu == 1) {
 	session_register('username');
 	session_register('id_user');
 	$_SESSION['id_user']=$id_user;
-	$_SESSION['username'] = $email;
+	$_SESSION['username'] = $username;
 	//$useragent = $_SERVER['HTTP_USER_AGENT'];
 	//$name = $_SESSION['full_name'];
 	//$query=mysql_query("update users set date_login=NOW(),ip='".$_SERVER['REMOTE_ADDR']."',status='on' , browser='".$_SERVER['HTTP_USER_AGENT']."' where uid='$uid' ");
