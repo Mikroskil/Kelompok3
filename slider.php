fff229e757f0a698154ce24d2616e268fa2116eb
josep-gaol <josephutagaol@gmail.com>
2014-01-27 00:42:44 +0700

    Create slider.php

---
+++ slider.php
@@ -0,0 +1,13 @@
+<section class="slider">
+		<div class="flexslider">
+			<ul class="slides">
+				<?php $i=1; while($i<=4){ ?>
+				<li>
+					<img src="images/slider/<?php echo $i; ?>.jpg" />
+				</li>
+				<?php $i++; } ?>
+			</ul>
+		</div>
+	</section>
+	
+	
