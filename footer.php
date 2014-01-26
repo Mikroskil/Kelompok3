3c71b60f091c2f705efe6b425b89ffd95617901f
Rama-Dhan <rama12dhan@rocketmail.com>
2014-01-27 01:03:55 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,17 +1,10 @@
 
-		<div class="footer">
-        	<div class="text">
-        	<p>&copy; Copyright 2013.&nbsp; PKTI - All Right Reserved </p>
-            </div>
-            <div class="socnet">
-            <a href="twiter.php"><img src="img/foto3.jpg"  class="twit"/></a>
-            </div>
-            </div>
-        		
-	<script src="./js/jquery-1.10.0.min.js"></script>
-	<script src="./js/bootstrap.min.js"></script>
-	<script src="./js/holder.js"></script>
-	<script src="./js/script.js"></script>
-    
-</body>
-</html>
+<div id="footer">
+    <div class="container">
+		<label >&copy; Copyright 2013.&nbsp; PKTI - All Right Reserved</label>
+		<label id="logo-footer"></label>
+		<div style="clear:both"></diV>
+    </div>
+</div>
+
+
