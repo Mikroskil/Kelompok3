a751ded92fdb21ebf9eb1e58a58f1bb8fb3688c4
Rama-Dhan <rama12dhan@rocketmail.com>
2014-01-27 01:05:53 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,66 +1,55 @@
 <?php
-include 'header.php';
+ob_start();
+session_start();
+
 ?>
-
-<!-- bagian isi -->
-	<div class="container">
-		<div class="row">
-			<div class="span3">
-				<?php
-				include 'sidebar.php';
-				?>
-			</div>
-
-			<div class="span9">
-				<div class="hero-unit" style=" background:none">
-                	<!-- start slide -->
-                    
-					<embed src="img/myfile1.swf" quality="high" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" class="foto" bgimage="img/bg_3.gif"></embed>
-                    
-				</div>
-
-				<ul class="thumbnails">
-                <?php
-                	$tampil = "select * from berita order by id_berita DESC limit 0,10";
-                  	$hasil  = mysql_query($tampil);
-                  	$no = $posisi+1;
-                  	while($row=mysql_fetch_array($hasil)){
-						
-				//$judul = preg_replace("/\s/","-",$row['judul']);
-    			//$url_link = "berita".$row['id_berita']."-".$judul.".html";
-						echo '<li class="span3">';
-						echo '<div class="thumbnail">';
-						echo'<img src="gambar/'."$row[gambar]".'" class="foto-wisata"> ';
-						echo '<div class="caption">';
-						echo '<h4><a href="detail_berita.php?id='."$row[id_berita]".'"> ' .$row['judul'].' </a></h4>';
-						echo '<p> '.substr("$row[isi]",0,50).' </p>';
-						echo '<a class="btn btn-primary" style="margin-left:160px" href="detail_berita.php?id='."$row[id_berita]".'">Detail...</a>';
-						echo '</div> </div> </li>';
-						
-				 }
-				 ?>
-				</ul>
-
-				<div class="pagination">
-					<ul>
-						<li class"disabled"><span>Prev</span></li>
-						<li class"disabled"><span>1</span></li>
-						<li><a href="#">2</a></li>
-						<li><a href="#">3</a></li>
-						<li><a href="#">4</a></li>
-						<li><a href="#">5</a></li>
-						<li><a href="#">Next</a></li>
-					</ul>
-				</div>
-
-			</div>
-		</div>
-        <!-- akhir isi -->
-	</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>INA FLASH</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+   	<link href="css/global.css" rel="stylesheet">
+	<link rel="stylesheet" href="plugins/flexslider.css" type="text/css" media="screen" />
 	
-	<hr />
+	
+	<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/global.js"></script>
+<script defer src="plugins/jquery.flexslider-min.js"></script>
+<script src="plugins/jquery.easing.js"></script>
+<script src="plugins/jquery.mousewheel.js"></script> 
+    
+</head>
 
-	<br>
-<?php
-include 'footer.php';
-?>
+<body>
+<?php include "header.php"; ?>
+
+<div class="container theme-showcase" id='isi' style="position:relative" >
+<?php include "content.php"; ?>
+</div>
+
+<?php include "footer.php"; ?>
+
+
+	 
+	 <script>
+
+$(document).ready(function()
+{
+	$('.flexslider').flexslider({
+		animation	: "slide",
+		controlNav	: false,
+		start		: function(slider){
+		$('body').removeClass('loading');
+		}
+		});
+		});
+</script>
+</body>
+</html>
