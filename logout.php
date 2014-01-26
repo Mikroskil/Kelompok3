fb2b29d00c1fcbc0993b55db3a22c51a1cbf0c96
josep-gaol <josephutagaol@gmail.com>
2014-01-27 00:55:56 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-$_SESSION=array();
 session_destroy();
-setcookie('PHPSESSID','',time()-3600,'/','',0);
-header("location: .");
+header("location:../index.php?module=home");
 ?>
