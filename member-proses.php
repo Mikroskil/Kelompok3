90703f830b07e664f29920e099e1a2d5f18fe320
josep-gaol <josephutagaol@gmail.com>
2014-01-27 00:44:06 +0700

    Update member-proses.php

--- member-proses.php
+++ member-proses.php
@@ -1,45 +1,52 @@
 <?php
-include 'konfigurasi/koneksi.php';
+include '../konfigurasi/koneksi.php';
 $username=$_REQUEST['username'];
 $nama=$_REQUEST['nama'];
 $password=$_REQUEST['pass'];
 $email=$_REQUEST['email'];
+$telp=$_REQUEST['telp'];
+$alamat=$_REQUEST['alamat'];
+$pertanyaan=$_REQUEST['pertanyaan'];
+$Jawab=$_REQUEST['Jawab'];
 $confirm=$_REQUEST['confirm'];
 
 if($username!="" && $password!="")
 {
-	if($password==$confirm)
+	
+	$query=mysql_query("select * from users where username='$username' ");
+	$row=mysql_fetch_array($query);
+	
+	if ($row['username']==$username)
+	{
+	echo 3;		
+	}
+	
+	else if($password==$confirm)
 	 {
-	  $password=md5($_REQUEST['pass']);
-		$qry=mysql_query("insert into users(username,password,nama,email) values ('$username','$password','$nama','$email')",$link) or die (mysql_error());
+		$password=md5($_REQUEST['pass']);
+		$qry=mysql_query("insert into users(username,password,nama,email,telp,alamat,foto_akun,pertanyaan,jawaban) values ('$username','$password','$nama','$email','$telp','$alamat','akunDefault.jpg','$pertanyaan','$Jawab')",$link) or die (mysql_error());
 		
-		 ?>
-		
-		 <script language="javascript">
-		<!--
-				alert('Terima kasih sudah mendaftar, silahkan login untuk masuk ke dashboard anda')
-				window.location = "index.php";
-				
-		--></script>
-		<?php
-		}
+	echo 0;		
+	}
 	else
 		{
-		?>
-		<script language="javascript">
+		 
+		echo 1;
+		/*<script language="javascript">
 		alert("Isi Confirm Password anda sesuai dengan Password anda")
 		window.location = "index.php";
 		
 		</script>
-		<?php
+		*/
 	 }
 }
 else
 {
-?>
+echo 2;
+/*
 <script language="javascript">
 window.location = "index.php";
 alert("Silahkan isi data anda dengan benar")
 </script>
-<?php
+*/
 	 }	 ?>
