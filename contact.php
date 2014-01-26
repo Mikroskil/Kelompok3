a2bb5a89cc43707b6fc90094f603c456b4c0533a
josep-gaol <josephutagaol@gmail.com>
2014-01-27 00:46:37 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,44 +1,22 @@
-<?php 
-include 'header.php';
-?>
-<!-- end header -->
-	<div class="container">
-		<div class="row">
-			<div class="span3">
-				<?php
-				include 'sidebar.php';
-				?>
-			</div>
-            
-			<div class="span9">
-				<div class="hero-unit">
-                	<h3>Contact Us</h3>
-                    <hr>
-                    <table border="0" width="100%">
-                    	<tr>
-                        	<td>Email</td>
-                            <td>:</td>
-                            <td>Dyanshb42@gmail.com</td>
-                        </tr>
-                        <tr>
-                        	<td>Telepon/Hp</td>
-                            <td>:</td>
-                            <td>087749379737</td>
-                        </tr>
-                        <tr>
-                        	<td>Alamat</td>
-                            <td>:</td>
-                            <td>Patumbak,jln Pertahanan</td>
-                        </tr>
-                    </table>
-                	
-				</div>
-			</div>
-		</div>
-	</div>
+<script> $(document).ready(function(){ hilang(); $('#l-contact').addClass('active').css("opacity","1");});</script>
+<div class="row" style="height:440px;">
 	
-	<hr />
-
-	<?php
-include 'footer.php';
-?>
+		<h2 style="border-bottom:1px solid rgba(0,0,0,.1);padding-bottom:10px;">Contact Us</h2>
+		<table border="0" width="100%">
+            <tr>
+                <td>Email</td>
+                <td>:</td>
+                <td>Brojolabest Team Development</td>
+            </tr>
+            <tr>
+                <td>Telepon/Hp</td>
+                <td>:</td>
+                <td>085361028107</td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td>Sibolga</td>
+            </tr>
+        </table>
+</div>
